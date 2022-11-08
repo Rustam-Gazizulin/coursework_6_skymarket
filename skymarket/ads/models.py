bdeb7272cd812042eed06ee5f59c46911f231e57
rgazizulin174 <rgazizulin174@gmail.com>
2022-11-08 20:52:35 +0500

    add model Ad, Custom

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,32 @@
-from django.conf import settings
 from django.db import models
 
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=100, verbose_name='Название товара')
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title 
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    text = models.TextField()
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.text
